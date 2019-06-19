ba48e733643fd4d17886813bcf3e9e66e3620611
Ben Tilghman <ben.tilghman@gmail.com>
2019-06-19 13:00:38 -0400

    Question 3

---
+++ dsa-search/src/Queue.js
@@ -0,0 +1,60 @@
+const _Node = require('./Node');
+
+
+class Queue {
+    constructor() {
+        this.first = null;
+        this.last = null;
+    }
+
+    enqueue(data) {
+        const node = new _Node(data);
+        if (this.first === null) {
+            this.first = node;
+        }
+        if (this.last) {
+            this.last.next = node;
+            node.prev = this.last;
+        }
+        this.last = node;
+    }
+
+    dequeue() {
+        if (this.first === null) {
+            return;
+        }
+        const node = this.first;
+
+        if (node === this.last) {
+            this.last = null;
+            this.first = null;
+        } else {
+            this.first = this.first.next;
+            this.first.prev = null;
+        }
+
+        return node.value;
+    }
+
+    peek(q) {
+        return q.first.value;
+    }
+
+    isEmpty(q) {
+        if (q.first === null) {
+            return true;
+        }
+        return false;
+    }
+
+    display(q) {
+        let currentChar = q.first;
+
+        while (currentChar !== null) {
+            console.log(currentChar.value);
+            currentChar = currentChar.next;
+        }
+    }
+}
+
+module.exports = Queue;
---
+++ dsa-search/src/Search.js
@@ -0,0 +1,87 @@
+const Queue = require('./Queue');
+
+class BinarySearchTree {
+    constructor(key = null, value = null, parent = null) {
+        this.key = key;
+        this.value = value;
+        this.parent = parent;
+        this.left = null;
+        this.right = null;
+    }
+
+    linearSearch(array, value) {
+
+        for (let i = 0; i < array.length; i++) {
+    
+            if (array[i] === value) {
+                console.log('Linear Search took', (i + 1), 'iterations');
+                return i;
+            }
+        }
+        console.log('Linear Search took', array.length, 'iterations');
+        return -1;
+    };
+
+    binarySearch(array, value, start, end, count = 0) {
+        start = start === undefined ? 0 : start;
+        end = end === undefined ? array.length : end;
+
+        if (start > end) {
+            console.log('Binary search took', count, 'iterations.');
+            return -1;
+        }
+
+        const index = Math.floor((start + end) / 2);
+        const item = array[index];
+
+        console.log('start:', start, 'end:', end);
+        if (item === value) {
+            count++;
+            console.log('Binary search took', count, 'iterations.')
+            return index;
+        }
+        else if (item < value) {
+            count++
+            console.log('count:', count, 'item:', item)
+            return this.binarySearch(array, value, index + 1, end, count);
+        }
+        else if (item > value) {
+            count++
+            console.log('count:', count, 'item:', item)
+            return this.binarySearch(array, value, start, index - 1, count);
+        }
+    };
+
+    dfs(values = []) {
+        if (this.left) {
+            values = this.left.dfs(values);
+        }
+        values.push(this.value);
+
+        if (this.right) {
+            values = this.right.dfs(values);
+        }
+        return values;
+    }
+
+    bfs(values = []) {
+        const queue = new Queue(); // Assuming a Queue is implemented (refer to previous lesson on Queue)
+
+        while (queue.length) {
+            const node = queue.dequeue(); //remove from the queue
+            values.push(node.value); // add that value from the queue to an array
+
+            if (node.left) {
+                queue.enqueue(node.left); //add left child to the queue
+            }
+
+            if (node.right) {
+                queue.enqueue(node.right); // add right child to the queue
+            }
+        }
+
+        return values;
+    }
+}
+
+module.exports = BinarySearchTree;
